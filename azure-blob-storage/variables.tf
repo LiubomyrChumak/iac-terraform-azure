727795aa89fb2cecb606424564705bc01d9eba01
Liubomyr Chumak <63431742+LiubomyrChumak@users.noreply.github.com>
2023-05-25 20:52:33 +0200

    Create variables.tf

---
+++ azure-blob-storage/variables.tf
@@ -0,0 +1,24 @@
+variable "resource_group_name" {
+  description = "Nazwa grupy zasobów"
+  type        = string
+}
+
+variable "location" {
+  description = "Lokalizacja zasobów"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Nazwa konta magazynowego"
+  type        = string
+}
+
+variable "storage_container_name" {
+  description = "Nazwa kontenera magazynu"
+  type        = string
+}
+
+variable "blob_name" {
+  description = "Nazwa bloba"
+  type        = string
+}
